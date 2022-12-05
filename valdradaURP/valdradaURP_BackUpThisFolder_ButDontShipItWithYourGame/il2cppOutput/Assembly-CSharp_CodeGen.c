1dfac460aebd958fc1424894039cca3577184459
DavinW <49922027+DavinWam@users.noreply.github.com>
2022-12-05 15:29:06 -0500

    refactored program

---
+++ valdradaURP/valdradaURP_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void animateHand::Start()
+extern void animateHand_Start_m69D02B949C566C4B871D9C59F2A6BF12F8510221 (void);
+// 0x00000002 System.Void animateHand::Update()
+extern void animateHand_Update_m8F156D7764169F8CE1564C13259604EB0E06F25E (void);
+// 0x00000003 System.Void animateHand::.ctor()
+extern void animateHand__ctor_m9FAD66B02447EB703508A579865EF5ED9BAF2C6A (void);
+// 0x00000004 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000005 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	animateHand_Start_m69D02B949C566C4B871D9C59F2A6BF12F8510221,
+	animateHand_Update_m8F156D7764169F8CE1564C13259604EB0E06F25E,
+	animateHand__ctor_m9FAD66B02447EB703508A579865EF5ED9BAF2C6A,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	6575,
+	6575,
+	6575,
+	6575,
+	6575,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
